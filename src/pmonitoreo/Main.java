87162a49a407113308d763ded0bc8c901cbe2315
abdiel_249 <abdielorellana3@gmail.com>
2020-12-11 10:43:13 -0400

    init project structure and packeges of project Plataforma de Monitoreo

---
+++ src/pmonitoreo/Main.java
@@ -0,0 +1,7 @@
+package pmonitoreo;
+
+import netscape.javascript.JSObject;
+
+public class Main {
+
+}
---
+++ src/pmonitoreo/cohabitante/Cohabitante.java
@@ -0,0 +1,16 @@
+package pmonitoreo.cohabitante;
+
+import javax.swing.JFrame;
+
+public class Cohabitante extends JFrame {
+
+  public Cohabitante(){
+
+    initCohabitante();
+  }
+
+  private void initCohabitante() {
+
+  }
+
+}
