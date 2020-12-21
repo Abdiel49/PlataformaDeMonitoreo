a26b344763af664590cfb699b1d49c0fca734420
abdiel_249 <abdielorellana3@gmail.com>
2020-12-20 23:32:48 -0400

    avance en la lectura de archivos pero la escritura no esta funcionando elimina los datos del archivo, probare con openCSVreader

--- src/pmonitoreo/backend/cohabitantes/CSVReader.java
+++ src/pmonitoreo/backend/cohabitantes/CSVReader.java
@@ -2,26 +2,28 @@ package pmonitoreo.backend.cohabitantes;
 
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
-
 import java.io.IOException;
 
+import java.nio.charset.Charset;
 import java.nio.file.Files;
-
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.nio.file.StandardOpenOption;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class CSVReader {
 
   private final String absolutePath;
+  private String fileId;
 
   public CSVReader() {
     absolutePath = "src/pmonitoreo/backend/cohabitantes/";
   }
 
-  public List<String[]> getData() {
+  public List<String[]> readData() {
 
     List <String[]> dataCSV = null;
     try {
@@ -32,7 +34,6 @@ public class CSVReader {
       while( (line = csvReader.readLine()) != null ){
         String[] data = line.split(",");
         dataCSV.add(data);
-        System.out.println(Arrays.toString(data));
       }
     }catch (FileNotFoundException e) {
       e.printStackTrace();
@@ -41,7 +42,20 @@ public class CSVReader {
     }
     return dataCSV;
   }
-  public void whiteData(){
+  public void saveCohabitanteStatus(int id, CondicionSanitaria condicionSanitaria){
+    String path = absolutePath + "cohabitantes.csv";
+    /*try {     // method with BufferedWriter -> Files.newBufferedWriter(Paths.get(_path_))
+      BufferedWriter writer = Files.newBufferedWriter(Paths.get(path), Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+      String[] datas = condicionSanitaria.getCondicionSanitaria();
+      String rowInfo = String.join(",", datas);
+      writer.write(rowInfo);
+      writer.newLine();
+      System.out.println("Se a escrito en el documento");
+    }catch (IOException e){
+      System.out.println(e);
+    }*/
 
+    // using CSVReader library
+    
   }
 }
--- src/pmonitoreo/backend/cohabitantes/CondicionSanitaria.java
+++ src/pmonitoreo/backend/cohabitantes/CondicionSanitaria.java
@@ -3,10 +3,18 @@ package pmonitoreo.backend.cohabitantes;
 public class CondicionSanitaria {
 
   private double Temperatura;
+  private String Date;
 
-  public CondicionSanitaria () {
-
+  public CondicionSanitaria (String date, double temp) {
+    this.Date = date;
+    this.Temperatura = temp;
   }
 
+  public String[] getCondicionSanitaria(){
+    return new String[]{
+        this.Date,
+        this.Temperatura+""
+    };
+  }
 
 }
